430b3d0ccdfcaf5d15fd6be6bed283ce4ca1815a
GitMew <thomas.bauwens@student.kuleuven.be>
2024-04-24 12:03:07 +0200

    File paths now detect editable install.

--- src/irse/general.py
+++ src/irse/general.py
@@ -1,7 +1,16 @@
+import os
 from pathlib import Path
-PATH_ROOT = Path(__file__).resolve().parent.parent
-PATH_DATA = PATH_ROOT / "data"
-PATH_DATA_IN = PATH_DATA / "in"
+PATH_PACKAGE = Path(__file__).resolve().parent
+PATH_SRC     = PATH_PACKAGE.parent
+IS_EDITABLE_INSTALL = PATH_SRC.stem == "src"
+
+if IS_EDITABLE_INSTALL:
+    PATH_ROOT = PATH_SRC.parent
+else:
+    PATH_ROOT = Path(os.getcwd())
+
+PATH_DATA     = PATH_ROOT / "data"
+PATH_DATA_IN  = PATH_DATA / "in"
 PATH_DATA_OUT = PATH_DATA / "out"
 PATH_DATA_OUT.mkdir(exist_ok=True, parents=True)
 
