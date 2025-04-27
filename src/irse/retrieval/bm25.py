019d80383c9fc758fddd9771ec80791ab537d8ee
Thomas Bauwens <thomas.bauwens@kuleuven.be>
2025-04-27 10:07:47 +0200

    Let's not push broken implementations, shall we.

--- src/irse/retrieval/bm25.py
+++ src/irse/retrieval/bm25.py
@@ -2,26 +2,30 @@ from typing import List, Iterable, Tuple
 
 import nltk
 from nltk.corpus import stopwords
-from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
 nltk.download("wordnet", quiet=True)
 nltk.download("stopwords", quiet=True)
 
-# from tktkt.preparation.mappers import Lowercaser, FilterCharacters, MapperSequence, Stripper
-# from tktkt.preparation.instances import TraditionalPretokeniser, Preprocessor, PunctuationPretokeniser
+from tktkt.preparation.mappers import Lowercaser, FilterCharacters, MapperSequence, Stripper
+from tktkt.preparation.instances import TraditionalPretokeniser, Preprocessor, PunctuationPretokeniser
 
 from rank_bm25 import BM25Okapi
 
-# SimpleNormaliser = MapperSequence([
-#     Stripper(),
-#     Lowercaser(),
-#     FilterCharacters(PunctuationPretokeniser.buildPunctuationString())
-# ])
+
+SimpleNormaliser = MapperSequence([
+    Stripper(),
+    Lowercaser(),
+    FilterCharacters(PunctuationPretokeniser.buildPunctuationString())
+])
+
 
 class OkapiRetrieval:
 
     def __init__(self, corpus: Iterable[str]):
-        self.pretokeniser = word_tokenize()
+        self.pretokeniser = Preprocessor(
+            uninvertible_mapping=SimpleNormaliser,
+            splitter=TraditionalPretokeniser()
+        )
         self.lemmatizer = WordNetLemmatizer()
         self.stopwords  = set(stopwords.words("english"))
 
